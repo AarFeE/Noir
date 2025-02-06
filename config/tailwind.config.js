be48007600049561cd2ddf3edb9ebcd1ee575caa
AarFeE <andres.jimenezv@hotmail.com>
2025-02-06 09:51:04 -0500

    fully implement flowbite

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,12 +1,23 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-    content: [
-      './app/views/**/*.html.erb',
-      './app/helpers/**/*.rb',
-      './app/javascript/**/*.js',
-      './app/assets/stylesheets/**/*.css',
-    ],
-    theme: {
-      extend: {},
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css',
+    './node_modules/flowbite/**/*.js' // Add Flowbite
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [
+    require('@tailwindcss/forms'), // Optional: for form styling
+    require('flowbite/plugin') // Add Flowbite plugin
+  ],
+};
